0930f9b69dde61e30a85d14d830241583a56990a
Subrat Kumar Singh <subratsinghsks_cse16@its.edu.in>
2024-07-09 04:15:17 +0000

    damemon and Child threads

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,31 @@
 import java.io.File;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.UserPrincipal;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import CountDownLatch.Worker;
 import Problems.Data;
-import javafx.scene.shape.Path;
+import threads.Task;
+
+
+
+
+
+
+
 
 public class Main {
 
 
-
-
     public static void main(String[] args) {
+      
 
-        File file = new File("/workspace/DesignPatternsJava/src/Problems/file.txt");
+   Thread a=new Thread(new Task());
+    
+   a.start();
 
-        System.out.println("Is exist: " + file.exists()); // Is exist: true
-        System.out.println("Is file: " + file.isFile()); // Is file: true
-        System.out.println("Name of file: " + file.getName());
-
-
-        
 
 
 
